59f7b36a6fb362b10490dc3621a9ddcaa27b78bf
Roglavsil <ravila@lcg.unam.mx>
2023-01-31 14:06:15 -0600

    Opinión paquete de R

--- R/03_ejercicio_bioc.R
+++ R/03_ejercicio_bioc.R
@@ -1,1 +1,14 @@
-## TODO
+# ccImpute
+
+# Marcin Malec
+
+# 2023-01-19
+
+# Se encarga de un método más efectivo para la selección de eventos dropout en
+# análisis single cell de RNA-seq.
+
+# Opinión
+# Me pareció bastante directo y sencillo, me gusta su simplicidad y la forma en
+# la que el autor explica su funcionamiento. Además tiene pocas dependencias.
+# Considero que tiene cosas a mejorar como el pre-procesamiento de los datos que
+# podría estar incluido en el paquete.
